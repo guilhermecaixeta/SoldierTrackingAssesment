29ad955b14061111720f4be1c0c9deb2ffd750b9
Guilherme Caixeta <guilherme.lpcaixeta@outlook.com>
2024-08-19 19:21:51 +0100

    Adding support to DI on WCF app

--- SoldierTrackingAssesment/App.xaml.cs
+++ SoldierTrackingAssesment/App.xaml.cs
@@ -1,14 +1,40 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Infra.SoldierTrackingAssesment;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
-namespace SoldierTrackingAssesment
+namespace SoldierTrackingAssesment;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private readonly ServiceProvider serviceProvider;
+
+    public App()
     {
+        ServiceCollection services = new ServiceCollection();
+        ConfigureServices(services);
+        serviceProvider = services.BuildServiceProvider();
     }
 
+    private void ConfigureServices(ServiceCollection services)
+    {
+        services.AddDbContext<SoldierTrackingAssesmentContext>(options => options
+            .UseSqlServer(@"Server=localhost;Database=SoldierTracking_development;ConnectRetryCount=0;Password=password123!;User Id=sa;TrustServerCertificate=True;",
+            builder => builder.UseNetTopologySuite()));
+
+        services.AddSingleton<MainWindow>();
+    }
+
+    private void OnStartup(object sender, StartupEventArgs e)
+    {
+        var dbContext = serviceProvider.GetRequiredService<SoldierTrackingAssesmentContext>();
+
+        dbContext.Database.Migrate();
+
+        var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
 }
---
+++ SoldierTrackingAssesment/Views/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Infra.SoldierTrackingAssesment;
+using System.Windows;
+
+namespace SoldierTrackingAssesment;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly SoldierTrackingAssesmentContext dbContext;
+
+    public MainWindow(SoldierTrackingAssesmentContext dbContext)
+    {
+        this.dbContext = dbContext;
+        InitializeComponent();
+        GetSoldiers();
+    }
+
+    private void GetSoldiers()
+    {
+        var soldiers = dbContext.Soldiers.ToList();
+        SoldierDataGrid.ItemsSource = soldiers;
+    }
+}
